1bdcb31fd721b675ff6a520a35f9908b752323f6
MasamichiKanakubo <renlijinjiubao808@gmail.com>
2024-04-10 14:23:37 +0900

    test

---
+++ js/sample.js
@@ -0,0 +1,12 @@
+// CSSクラスが namebox である要素をクリックした時にその内容をアラートボックスに出力する．
+document.querySelector('.namebox').addEventListener('click', (event) => {
+  const text = event.target.innerText;
+  // アラートボックスに出力する．
+  alert(text);
+});
+
+// JavaScriptのプログラムを追加する場合，下に書いてください．
+/* ここから */
+
+
+/* ここまで */
